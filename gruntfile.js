94738098c86f7c4b34851a2b33562f4182194d71
wvffle <casper@wvffle.net>
2017-04-16 19:20:00 +0200

    build readme

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = (grunt) => {
   const json = grunt.file.readJSON('package.json');
+  const {spawn} = require('child_process');
   const ejs = require('ejs');
 
   grunt.initConfig({
@@ -7,7 +8,6 @@ module.exports = (grunt) => {
   });
 
   grunt.registerTask('arrchat', function() {
-    const done = this.async();
     const cwd = 'commands/';
     const files = grunt.file.expand({ cwd }, ['*.js']);
     const data = {
@@ -37,5 +37,9 @@ module.exports = (grunt) => {
     grunt.file.write('cli/arrchat.js', '#!/usr/bin/env node\n' + ejs.render(cli, data, { escape: s => s }));
   });
 
-  grunt.registerTask('default', [ 'arrchat' ]);
+  grunt.registerTask('readme', function() {
+    spawn('npm', ['run', 'readme']);
+  });
+
+  grunt.registerTask('default', [ 'arrchat', 'readme' ]);
 }
--- readme.md
+++ readme.md
@@ -1,20 +1,16 @@
 # webclient-cli
 ```
+
   Usage: arrchat [options] [command]
 
 
   Commands:
 
-    controller <name>  creates new controller
-    generator <name>   creates new generator
-    plugin <name>      creates new plugin
-    service <name>     creates new service
-    style <name>       creates new style
-    template <name>    creates new template
-    view <name>        creates new view
+    view <name>  creates new view
 
   Options:
 
     -h, --help     output usage information
     -V, --version  output the version number
+
 ```
